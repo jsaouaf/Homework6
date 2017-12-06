7a2578e01465da40eec1ce74c488b372d2af5504
unknown <zyud@seas.upenn.edu>
2017-12-06 14:34:26 -0500

    updated MainTester.java with serialization and deserialization tests

--- src/MainTester.java
+++ src/MainTester.java
@@ -1,3 +1,9 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * This class is for testing out classes
  * @author zyud
@@ -31,6 +37,41 @@ public class MainTester {
         
         // Adding Ajit Pai to task "Harvest Poop"
         verizon.addEmployeeToTask(1, 2);
+        
+        // Testing Serialization
+		try {
+	          FileOutputStream fileOut =
+	          new FileOutputStream("company.ser");
+	          ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	          out.writeObject(verizon);
+	          out.close();
+	          fileOut.close();
+	          System.out.println("Serialized data is saved in \"company.ser\"");
+	       } catch (IOException i) {
+	          System.out.println("IOException");
+	          System.out.println(i.getMessage());
+	       }
+        
+        // Testing Deserialization
+	  	 Company verizonRestored = null;
+		
+	  	 	  try {
+		         FileInputStream fileIn = new FileInputStream("company.ser");
+		         ObjectInputStream in = new ObjectInputStream(fileIn);
+		         verizonRestored = (Company) in.readObject();
+		         in.close();
+		         fileIn.close();
+		      } catch (IOException i) {
+		    	 System.out.println("IOException");
+		    	 System.out.println(i.getMessage());
+		      } catch (ClassNotFoundException c) {
+		         System.out.println("Company class not found");
+		         System.out.println(c.getMessage());
+		      }
+	      
+	      verizonRestored.printAllProjects();
+	      verizonRestored.printAllEmployees();
+        
     }
 
 }
