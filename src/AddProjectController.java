1e9347fd545377dba02242566ea4c35932ca3ece
cgui <cathleenyg@gmail.com>
2017-12-09 11:27:29 -0500

    update of project controller and css stylesheet

--- src/AddProjectController.java
+++ src/AddProjectController.java
@@ -24,13 +24,22 @@ public class AddProjectController {
 	private TextField deadline;
 
 //Need to handle changing the start/end/deadline dates in this method still if user chooses to enter them
-	public void addProjectClicked(ActionEvent event){
+	public void addProjectClicked(ActionEvent event) throws IOException{
 
 		Company.getInstance().newProject(projectName.getText());
 
 //		if(!startDate.getText().isEmpty()){
 //
 //		}
+
+		Stage primaryStage =  (Stage) ((Node) event.getSource()).getScene().getWindow();
+		primaryStage.setTitle("Syme");
+
+		Parent root = FXMLLoader.load(getClass().getResource("DashboardView2.fxml"));
+		Scene scene = new Scene(root);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public void dashboardClicked(ActionEvent event) throws IOException{
